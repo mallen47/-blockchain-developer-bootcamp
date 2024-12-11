9a0da928f766d395b301567f40b6782c57f5d85e
Matt Allen <allen.matt@gmail.com>
2024-12-11 06:57:39 -0500

    4. Token Balances

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -9,6 +9,8 @@ contract Token {
      uint256 public decimals = 18;
      uint256 public totalSupply;
 
+     mapping(address => uint256) public balanceOf;
+
      constructor(
         string memory _name,
         string memory _symbol,
@@ -17,7 +19,8 @@ contract Token {
         name = _name;
         symbol = _symbol;
         totalSupply = _totalSupply * (10**decimals);
-     } 
+        balanceOf[msg.sender] = totalSupply;
+     }
 
 }
  
