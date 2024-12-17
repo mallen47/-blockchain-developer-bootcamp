c7b51419bb06d5cecebb9399d73718d52f4b820e
Matt Allen <allen.matt@gmail.com>
2024-12-17 14:08:22 -0500

    update config and deploy script for deployment to testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,19 +1,24 @@
-require('@nomicfoundation/hardhat-toolbox')
-require('dotenv').config()
+require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config();
+const privateKeys = process.env.PRIVATE_KEYS || '';
 
-task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners()
+// task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
+//   const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(accounts.address)
-  }
-})
+//   for (const account of accounts) {
+//     console.log(accounts.address);
+//   }
+// });
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: '0.8.19',
   networks: {
     localhost: {},
+    sepolia: {
+      url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: privateKeys.split(','),
+    },
   },
   formatter: {
     enabled: true,
@@ -28,4 +33,4 @@ module.exports = {
       },
     },
   },
-}
+};
--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,16 +1,16 @@
 async function main() {
-    // Fetch contract to deploy
-    const Token = await ethers.getContractFactory("Token");
-    
-    // Deploy contract
-    const token = await Token.deploy();
-    await token.deployed();
-    console.log(`Token deployed to: ${token.address}`);
+  // Fetch contract to deploy
+  const Token = await ethers.getContractFactory('Token');
+
+  // Deploy contract
+  const token = await Token.deploy('Moon Lambo', 'MNB', '1000000');
+  await token.deployed();
+  console.log(`Token deployed to: ${token.address}`);
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);
-    })
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
